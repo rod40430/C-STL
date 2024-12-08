d8a54264b1bde4e20b62ba325150234c1ece41ea
xogns <xogns40430@naver.com>
2024-12-08 18:45:48 +0900

    Update 연산자 오버로딩 정의 및 사용하기.cpp

--- 연산자 오버로딩/연산자 오버로딩 정의 및 사용하기.cpp
+++ 연산자 오버로딩/연산자 오버로딩 정의 및 사용하기.cpp
@@ -10,17 +10,25 @@ private:
 public:
     Point(int x = 0, int y = 0) : x(x), y(y) {}
     void Print() const { cout << x << ',' << y << nl; }
-    void operator+ (Point arg)
+    const Point operator+ (const Point &arg)
     {
-        cout << "operator+() 함수 호출" << nl;
+        Point pt;
+        pt.x = this->x + arg.x;
+        pt.y = this->y + arg.y;
+
+        return pt;
     }
 };
 
 int main()
 {
     Point p1(2, 3), p2(5, 5);
+    Point p3;
 
-    p1 + p2;    // => p1.operator+(p2); 와 같습니다.
+    p3 = p1 + p2;    // 컴파일러가 p1.operator+(p2) 호출
+    p3.Print();
+    p3 = p1.operator+(p2); // 직접 호출
+    p3.Print();
     
     return 0;
 }
