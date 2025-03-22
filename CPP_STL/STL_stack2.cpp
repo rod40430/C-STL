f77e1de2613375fe9bca6735fd9da02a98a1ef59
xogns <xogns40430@naver.com>
2025-03-22 19:20:48 +0900

    stack에 vector 컨테이너를 적용한다

---
+++ CPP_STL/STL_stack2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#define nl '\n'
+
+using namespace std;
+
+int main() 
+{
+    stack<int, vector<int>> st;
+
+    st.push(10);
+    st.push(20);
+    st.push(30);
+
+    cout<<st.top()<<nl;
+    st.pop();
+
+    cout<<st.top()<<nl;
+    st.pop();
+
+    cout<<st.top()<<nl;
+    st.pop();
+
+    if (st.empty())
+        cout<<"stack에 데이터 없음"<<nl;
+    
+    return 0;
+}
